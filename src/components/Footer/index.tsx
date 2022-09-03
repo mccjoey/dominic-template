04fcbde25b4ff4d591074cd01103014af5562d96
Jose Sampaio <crash.oliver@gmail.com>
2022-09-02 23:00:51 -0300

    Footer animation

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,10 +2,25 @@ import styles from "./styles.module.scss";
 import { BiHomeSmile } from "react-icons/bi";
 import { MdLiveTv } from "react-icons/md";
 import { BsPerson, BsBag, BsDot } from "react-icons/bs";
+import { useCallback, useEffect, useState } from "react";
 
 export const Footer: React.FC = () => {
+  const [viewPosition, setViewPosition] = useState<string>("show");
+  const getViewPosition = useCallback(() => {
+    if (window.scrollY >= 450) {
+      setViewPosition("disable");
+    } else {
+      setViewPosition("show");
+    }
+  }, [viewPosition]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", getViewPosition, false);
+    return () => window.removeEventListener("scroll", getViewPosition, false);
+  }, []);
+
   return (
-    <footer className={styles.footer}>
+    <footer className={`${styles.footer} ${styles[viewPosition]}`}>
       <div className={styles.content}>
         <nav>
           <li className={`${styles.footerMenuItem} ${styles.active}`}>
