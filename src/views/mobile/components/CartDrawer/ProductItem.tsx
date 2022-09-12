811ad2c709efcbc192db7c36eb6b1ada51d194f7
Mccjoey <crash.oliver@gmail.com>
2022-09-12 19:59:16 -0300

    Cart Update

--- src/views/mobile/components/CartDrawer/ProductItem.tsx
+++ src/views/mobile/components/CartDrawer/ProductItem.tsx
@@ -1,20 +1,20 @@
 import Image from "next/future/image";
 import styles from "./styles.module.scss";
 import { AiFillCaretLeft, AiFillCaretRight } from "react-icons/ai";
-import { GrClose} from "react-icons/gr";
+import { GrClose } from "react-icons/gr";
 import { useCallback, useState } from "react";
 
 export const ProductItem = () => {
   const [productQuantity, setProductQuantity] = useState(1);
 
   const incrementQuantity = useCallback(() => {
-    setProductQuantity(state => state + 1)
-  }, [])
+    setProductQuantity((state) => state + 1);
+  }, []);
 
   const decrementQuantity = useCallback(() => {
-    if(productQuantity == 0) return;
-    setProductQuantity(state => state  -1)
-  }, [productQuantity])
+    if (productQuantity == 0) return;
+    setProductQuantity((state) => state - 1);
+  }, [productQuantity]);
 
   return (
     <div className={styles.productItem}>
@@ -25,20 +25,20 @@ export const ProductItem = () => {
         height={2700}
         alt="banner"
       />
-      <GrClose className={styles.removeProduct}/>
+      <GrClose className={styles.removeProduct} />
       <div className={styles.productInfo}>
-        <p className={styles.productName}>Blazer Linho Alongado - Uva - P</p>
-        <div className={styles.productQuantity}>
-          <p className={styles.value}>
-            Qtd
-            <span>
-              <AiFillCaretLeft onClick={decrementQuantity} />
-              {productQuantity}
-              <AiFillCaretRight onClick={incrementQuantity} />
-            </span>
-          </p>
-          <p className={styles.productPrice}>R$299,99</p>
-        </div>
+        <p className={styles.productName}>Blazer Linho Alongado</p>
+        <p className={styles.productColor}>Cor: Uva</p>
+        <p className={styles.productSize}>Tamanho: P</p>
+        <p className={styles.productQuantity}>
+          Qtd
+          <span>
+            <AiFillCaretLeft onClick={decrementQuantity} />
+            {productQuantity}
+            <AiFillCaretRight onClick={incrementQuantity} />
+          </span>
+        </p>
+        <p className={styles.productPrice}>R$299,99</p>
       </div>
     </div>
   );
--- src/views/mobile/components/CartDrawer/index.tsx
+++ src/views/mobile/components/CartDrawer/index.tsx
@@ -2,6 +2,7 @@ import styles from "./styles.module.scss";
 import Drawer from "react-modern-drawer";
 import "react-modern-drawer/dist/index.css";
 import { ProductItem } from "./ProductItem";
+import { GrClose } from "react-icons/gr";
 
 interface CartDrawerProps {
   isOpen: boolean;
@@ -15,11 +16,13 @@ export const CartDrawer: React.FC<CartDrawerProps> = ({
   return (
     <Drawer open={isOpen} onClose={setIsOpen} direction="right" size="95%">
       <section className={styles.cartContent}>
+        <GrClose
+          className={styles.closeModal}
+          onClick={() => setIsOpen(!isOpen)}
+        />
         <header>
-          <p className={styles.title}>Carrinho</p>
-          <p className={styles.closeButton} onClick={() => setIsOpen(!isOpen)}>
-            Fechar
-          </p>
+          <p className={styles.title}>Lista de desejos (1)</p>
+          <p className={`${styles.title} ${styles.active}`}>Carrinho (1)</p>
         </header>
         <div className={styles.productsList}>
           {Array.from({ length: 3 }).map((product, index) => (
@@ -28,8 +31,9 @@ export const CartDrawer: React.FC<CartDrawerProps> = ({
         </div>
         <footer>
           <div className={styles.subtotal}>
-            <p>Subtotal:</p>
-            <p>R$299,00</p>
+            <p>
+              Subtotal:<span>R$299,00</span>
+            </p>
           </div>
           <div className={styles.buttons}>
             <button>Ver carrinho</button>
